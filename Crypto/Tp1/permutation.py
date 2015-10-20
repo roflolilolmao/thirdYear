328fc3006ffea19ea97c3b40b8bb53fcfe7fff3e
roflolilolmao <quentin.jeanmonod@he-arc.ch>
2015-10-20 13:50:04 +0200

    salut la compagnie

--- Crypto/Tp1/permutation.py
+++ Crypto/Tp1/permutation.py
@@ -3,14 +3,14 @@ __author__ = 'Quentin Jeanmonod'
 
 M = '0000000100100011010001010110011110001001101010111100110111101111'
 IP = [
-    58, 50, 42, 34, 26, 18, 10, 2,
-    60, 52, 44, 36, 28, 20, 12, 4,
-    62, 54, 46, 38, 30, 22, 14, 6,
-    64, 56, 48, 40, 32, 24, 16, 8,
     57, 49, 41, 33, 25, 17, 9, 1,
     59, 51, 43, 35, 27, 19, 11, 3,
     61, 53, 45, 37, 29, 21, 13, 5,
-    63, 55, 47, 39, 31, 23, 15, 7
+    63, 55, 47, 39, 31, 23, 15, 7,
+    56, 48, 40, 32, 24, 16, 8, 0,
+    58, 50, 42, 34, 26, 18, 10, 2,
+    60, 52, 44, 36, 28, 20, 12, 4,
+    62, 54, 46, 38, 30, 22, 14, 6
 ]
 
 
@@ -18,21 +18,19 @@ def inverseVector(vector):
     i = 0
     vector_I = []
     while i < len(vector):
-        i += 1
         vector_I.append(vector.index(i))
+        i += 1
     return vector_I
 
 
 def permutation(message, vector):
     assert(len(message) == len(vector))
     i = 0
-    Mp = {}
-    while i < len(message):
-        Mp[vector[i]] = message[i]
-        i += 1
+    Mp = []
     strMp = ''
-    for key in Mp:
-        strMp += str(Mp[key])
+    while i < len(message):
+        strMp += message[vector[i]]
+        i += 1
     return strMp
 
 
