8c0e5a9d9d32def9f39d9bf269ed3008a8232413
roflolilolmao <quentin.jeanmonod@he-arc.ch>
2015-12-05 10:39:54 +0100

    the great streak

--- notrelated/fuck.py
+++ notrelated/fuck.py
@@ -4,8 +4,12 @@ import numpy as np
 import urllib
 import struct
 import binascii
+
+
 def getc(s):
     return urllib.urlopen('http://www.pythonchallenge.com/pc/' + s).read()
+
+
 def fouslamerde(h):
     split = np.array_split(h, h.shape[0])
     print(len(split))
@@ -15,6 +19,7 @@ def fouslamerde(h):
         split[i], split[j] = split[j], split[i]
     new = np.concatenate(split)
     return new
+
 
 def e11():
     img = cv2.imread('cave.jpg')
@@ -31,15 +36,29 @@ def e11():
     cv2.imshow('ew', new)
     cv2.imshow('e2w', new2)
     cv2.waitKey()
-def e12():
-    # t = getc('return/evil2.gfx')
-    with open('evil2.gfx', 'rb') as f:
+
+
+def e12fuck(fil, n):
+    # dis pro port ional /ity
+    with open(fil, 'rb') as f:
         t = f.read()
         value = [struct.unpack('B', t[i])[0] for i in range(len(t))]
-        l = [''.join([chr(x) for x in value[i:len(value):5]]) for i in range(5)]
-        for i in range(5):
+        l = [''.join([chr(x) for x in value[i:len(value):n]]) for i in range(n)]
+        for i in range(n):
             with open('e{}.png'.format(i), 'wb') as fuck:
                 fuck.write(l[i])
-        
-        
-e12()
+
+
+def e12():
+    t = getc('return/evil2.gfx')
+    e12fuck('evil2.gfx', 5)
+
+
+def e13():
+    img = cv2.imread('disprop.jpg')
+    img = (255 - img)
+    cv2.imshow('peutetre', img)
+    cv2.waitKey()
+
+
+e13()
